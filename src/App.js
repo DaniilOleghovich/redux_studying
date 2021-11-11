44257fcb8ae4453ed7345d7c341b5bcbfd91a5eb
DaniilOleghovich <danya.pavlichenko.1997@mail.ru>
2021-11-11 11:59:18 +0200

    Added store, reducer, state and two actions to the start redux application. Also added two buttons that can add or get some cash from store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    const dispatch = useDispatch();
+    const cash = useSelector(state => state.cash);
+
+    const addCash = (cash) => {
+        dispatch({type: "ADD_CASH", payload: cash});
+    }
+
+    const getCash = (cash) => {
+        dispatch({type: "GET_CASH", payload: cash})
+    }
+
+    return (
+        <div className="App">
+            <h1>{cash}</h1>
+            <div style={{display: 'flex'}}>
+                <button onClick={() => addCash(Number(prompt()))}>Add cash</button>
+                <button onClick={() => getCash(Number(prompt()))}>Get cash</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
