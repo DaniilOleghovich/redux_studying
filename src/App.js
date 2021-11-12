37b738eb642fb0550db2d6483b23cb4258a6576a
DaniilOleghovich <danya.pavlichenko.1997@mail.ru>
2021-11-13 01:04:40 +0200

    Refactored code. Added ability to add new customer and also remove customer when clicked on it. Also added action creators functions inside reducers, that helps to pass data into dispatch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
+import {addCustomerAction, removeCustomerAction} from "./store/customerReducer";
+import {addCashAction, getCashAction} from "./store/cashReducer";
 
 function App() {
 
     const dispatch = useDispatch();
     const cash = useSelector(state => state.cash.cash);
+    const customers = useSelector(state => state.customers.customers);
 
     const addCash = (cash) => {
-        dispatch({type: "ADD_CASH", payload: cash});
+        if (Number.isNaN(cash) && cash !== undefined) {
+            dispatch(addCashAction(0));
+            addCash(Number(prompt()));
+        } else {
+            dispatch(addCashAction(cash));
+        }
     }
 
     const getCash = (cash) => {
-        dispatch({type: "GET_CASH", payload: cash})
+        if (Number.isNaN(cash) && cash !== undefined) {
+            dispatch(getCashAction(0));
+            getCash(Number(prompt()));
+        } else {
+            dispatch(getCashAction(cash));
+        }
+    }
+
+    const addCustomer = (name) => {
+        const customer = {
+            name: name,
+            id: Date.now()
+        }
+        dispatch(addCustomerAction(customer));
+    }
+
+    const removeCustomer = (customer) => {
+        dispatch(removeCustomerAction(customer.id));
     }
 
     return (
         <div className="App">
-            <h1>{cash}</h1>
-            <div style={{display: 'flex'}}>
-                <button onClick={() => addCash(Number(prompt()))}>Add cash</button>
-                <button onClick={() => getCash(Number(prompt()))}>Get cash</button>
+            <div className="wrap" style={{display: 'flex'}}>
+                { !Number.isNaN(cash) ?
+                    <div>
+                        <h1>Balance: {cash}</h1>
+                    </div>
+                    :
+                    <div>
+                        <h1>Please type a number value</h1>
+                    </div>
+                }
+                <div>
+                    <button onClick={() => addCash(Number(prompt()))}>Add cash</button>
+                    <button onClick={() => getCash(Number(prompt()))}>Get cash</button>
+                    <button style={{width: "200px"}} onClick={() => addCustomer(prompt())}>Add Customer</button>
+                </div>
             </div>
+            { customers.length > 0 ?
+                <div>
+                    {customers.map(customer =>
+                        <div onClick={() => removeCustomer(customer)} key={customer.id} className="customer">
+                            {customer.name}
+                        </div>
+                    )}
+                </div>
+                :
+                <div style={{fontSize: "1.5rem", marginTop: "10px"}}>
+                    Customers are not exists!
+                </div>
+            }
         </div>
     );
 }
--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -2,13 +2,21 @@ const defaultState = {
     customers: []
 }
 
+const ADD_CUSTOMER = "ADD_CUSTOMER";
+const REMOVE_CUSTOMER = "REMOVE_CUSTOMER";
+
 export const customerReducer = (state = defaultState, action) => {
     switch (action.type) {
-        case "ADD_CUSTOMER":
-            return {...state, cash: state.cash + action.payload}
-        case "GET_CUSTOMER":
-            return {...state, cash: state.cash - action.payload}
+        case ADD_CUSTOMER:
+            return {...state, customers: [...state.customers, action.payload]}
+        case REMOVE_CUSTOMER:
+            return {...state, customers: state.customers.filter(customer => customer.id !== action.payload)}
         default:
             return state;
     }
 }
+
+// This functions calls "action creators". They are help when you use dispatch to put some data to reducers.
+// You need call someone of this functions into dispatch and put them payload data only, without type
+export const addCustomerAction = (payload) => ({type: ADD_CUSTOMER, payload});
+export const removeCustomerAction = (payload) => ({type: REMOVE_CUSTOMER, payload});
